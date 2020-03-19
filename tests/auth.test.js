a22a846aa881c268a30718ff855fe461aa53fd50
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2020-03-18 18:26:25 -0700

    Removed commented out code

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,12 +1,6 @@
-// const esmImport = require('esm')(module);
 import server from '../server.js';
 import request from 'supertest';
 import db from '../config/dbConfig';
-
-// const request = esmImport("supertest");
-// const server = esmImport("../server");
-
-// const db = esmImport("../config/dbConfig.js");
 
 afterAll(async () => {
  await db.raw(`TRUNCATE TABLE users RESTART IDENTITY CASCADE`);
@@ -31,37 +25,7 @@ describe("register user", () => {
     expect(res.body.id).not.toBeNaN();
 
   })
-
-  // beforeEach(async () => {
-  //   await db("users").truncate();
-  // });
  });
-
-//   it("should return json", async () => {
-//     const res = await request(server)
-//       .post("/api/register")
-//       .send({
-//        first_name: "bart", last_name: "kofman", email: "bart2@gmail.com",
-//         password: "password"
-//       });
-//     expect(res.type).toBe("application/json");
-//   })
-
-
-//   it("returns an id", async () => {
-//     const res = await request(server)
-//       .post("/api/register")
-//       .send({
-//        first_name: "bart", last_name: "kofman", email: "bart3@gmail.com",
-//         password: "password"
-//       });
-//     expect(res.body.id).not.toBeNaN();
-//   });
-
-//   beforeEach(async () => {
-//     await db("users").truncate();
-//   });
-// });
 
 describe("login works", () => {
   it("should return status 200", async () => {
