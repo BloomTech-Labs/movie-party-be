7b7c92ff0e683d76b44570c91956561426dd1b47
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2020-04-09 22:04:43 -0700

    Added GET & POST

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const usersRouter = require("./routes/users-router.js");
 const registerRouter = require("./routes/register-router.js");
 const eventRouter = require("./routes/event-route.js");
 const memberRoute = require("./routes/event-member-router.js");
+const confirmedRoute = require("./routes/confirmed-router.js");
 
 // Middleware
 server.use(cors());
@@ -19,6 +20,7 @@ server.use("/api/register", registerRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/event", eventRouter);
 server.use("/api/member", memberRoute);
+server.use("/api/confirmed", confirmedRoute);
 
 //Routes
 server.get("/", (req, res) => {
